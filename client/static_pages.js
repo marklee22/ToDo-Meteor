fb201ac0e226200596b82efcc6be115b7726b6b1
Mark Lee <marklee22@gmail.com>
2013-05-26 12:09:53 -0700

    Adding router and login pages

---
+++ client/static_pages.js
@@ -0,0 +1,22 @@
+/* Page Routes */
+Meteor.Router.add({
+  '/': function() {
+    if(Meteor.user()) return 'todo_page';
+    else return 'login_page';
+  }
+});
+
+/* Filters to run before rendering a page */
+Meteor.Router.filters({
+  // Verify user is logged in or redirect to login page
+  'checkLoggedIn': function(page) {
+    if(Meteor.loggingIn()) return 'loading_page';
+    else if(Meteor.user()) return page;
+    else return 'login_page';
+  }
+});
+
+/* Determines whether to show the header in the app */
+Template.header_view.logged_in = function() {
+  return Meteor.user();
+};
